8c551d2099c3153265c57fb4314aa45ad55d3499
Alessio Mereghetti <amereghe@Ubuntu22.04>
2023-01-02 17:05:46 +0100

    FindMaxes.sh updated with new dose USRBIN. In addition, it loops over all necessary USRBINs

--- FindMaxes.sh
+++ FindMaxes.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-# bnnFile="../data_2022-11-11/P_W/P_W_48.bnn"
-# outFileName="AMB74_P_W_max_T%02i.dat"
-# bnnFile="../data_2022-11-11/P_Cu/p_Cu_48.bnn"
-# outFileName="AMB74_P_Cu_max_T%02i.dat"
-# bnnFile="../data_2022-11-11/P_Ni/P_Ni_48.bnn"
-# outFileName="AMB74_P_Ni_max_T%02i.dat"
-# bnnFile="../data_2022-11-11/P_brass/P_brass_48.bnn"
-# outFileName="AMB74_P_Brass_max_T%02i.dat"
-# bnnFile="../data_2022-11-18/C_W/C_W_48.bnn"
-# outFileName="AMB74_C_W_max_T%02i.dat"
-# bnnFile="../data_2022-11-18/C_Cu/C_Cu_48.bnn"
-# outFileName="AMB74_C_Cu_max_T%02i.dat"
-# bnnFile="../data_2022-11-18/C_Ni/C_Ni_48.bnn"
-# outFileName="AMB74_C_Ni_max_T%02i.dat"
-bnnFile="../data_2022-11-18/C_brass/C_brass_48.bnn"
-outFileName="AMB74_C_Brass_max_T%02i.dat"
+myConf=P_Ni
+bnnFiles=(
+    XPRcolli_48.bnn
+    XPRcolli_53.bnn
+    XPRcolli_54.bnn
+)
+outFileNames=(
+    "AMB74_${myConf}_max_T%02i.dat"
+    "AMB74_${myConf}_max_fingerAll_T%02i.dat"
+    "AMB74_${myConf}_max_fingerLep_T%02i.dat"
+)
 
 for ((ii=1;ii<=8;ii++)) ; do
-    outFile=`printf "${outFileName}" "${ii}"`
-    /usr/local/FLUKA_INFN/2021.2.7/flutil/usbmax -d ${ii} -a Z -o ${outFile} ${bnnFile}
-    # echo ${outFile}
+    for ((jj=0;jj<${#outFileNames[@]};jj++)) ; do
+        outFile=`printf "${outFileNames[$jj]}" "${ii}"`
+        ${FLUKA}/flutil/usbmax -d ${ii} -a Z -o ${outFile} ${bnnFiles[$jj]}
+        # echo ${outFile}
+    done
 done
